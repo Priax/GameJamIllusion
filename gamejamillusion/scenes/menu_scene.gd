52a74e942093342946ace66efda707da534c7d3d
finaud maxime <maxime.finaud@epitech.eu>
2025-02-08 18:04:11 +0100

    add a lot

--- gamejamillusion/scenes/menu_scene.gd
+++ gamejamillusion/scenes/menu_scene.gd
@@ -1,10 +1,8 @@
 extends Control
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -15,8 +13,7 @@ func startPressed() -> void:
 	
 
 func optionPressed() -> void:
-	print("Option Button")
-
+	get_tree().change_scene_to_file("res://scenes/option_scene.tscn")
 
 func exitPressed() -> void:
 	get_tree().quit()
---
+++ gamejamillusion/scenes/option_scene.gd
@@ -0,0 +1,44 @@
+extends Control
+
+var sfx_bus = AudioServer.get_bus_index("SFX")
+var master_bus = AudioServer.get_bus_index("Master")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func BackPresse() -> void:
+	get_tree().change_scene_to_file("res://scenes/menu_scene.tscn")
+
+func checkFullScreen(toggled_on: bool) -> void:
+	if toggled_on == true:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+func changeValueMaster(value: float) -> void:
+	AudioServer.set_bus_volume_db(master_bus, value)
+	
+	if value == -30:
+		AudioServer.set_bus_mute(master_bus, true)
+	else:
+		AudioServer.set_bus_mute(master_bus, false)
+
+
+func changeValueSFX(value: float) -> void:
+	AudioServer.set_bus_volume_db(sfx_bus, value)
+	
+	if value == -30:
+		AudioServer.set_bus_mute(sfx_bus, true)
+	else:
+		AudioServer.set_bus_mute(sfx_bus, false)
+
+func soundPresse_aaaa() -> void:
+	$Aaaaaaaa.play()
+
+func soundPresseNoir() -> void:
+	$"Allez-le-noir".play()
