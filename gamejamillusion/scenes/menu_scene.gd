92c4c5d985e312f04d4706dd95614d103e4a1772
finaud maxime <maxime.finaud@epitech.eu>
2025-02-08 21:14:08 +0100

    big intro

--- gamejamillusion/scenes/menu_scene.gd
+++ gamejamillusion/scenes/menu_scene.gd
@@ -1,16 +1,7 @@
 extends Control
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 func startPressed() -> void:
-	get_tree().change_scene_to_file("res://menu_scene.tscn")
-	
+	get_tree().change_scene_to_file("res://scenes/menu_scene.tscn")
 
 func optionPressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/option_scene.tscn")
--- gamejamillusion/scenes/option_scene.gd
+++ gamejamillusion/scenes/option_scene.gd
@@ -3,14 +3,6 @@ extends Control
 var sfx_bus = AudioServer.get_bus_index("SFX")
 var master_bus = AudioServer.get_bus_index("Master")
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 func BackPresse() -> void:
 	get_tree().change_scene_to_file("res://scenes/menu_scene.tscn")
 
